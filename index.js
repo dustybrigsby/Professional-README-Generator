13af0c9f9308fcaa21119cf783306ab352069095
Dusty Brigsby <108590519+dustybrigsby@users.noreply.github.com>
2023-07-03 07:43:32 -0700

    Update index.js
    Added writeToFile function, initialized it at the top of the init function

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const questions = [
         type: 'list',
         name: 'license',
         message: 'What license is used?',
-        choices: ['None','Apache License 2.0','GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal','Eclipse Public License 2.0','GNU Affero General Public License v2.0','GNU General Public License v2.0','GNU Lesser General Public License v2.1','Mozilla Public License 2.0','The Unlicense']
+        choices: [ 'None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause "Simplified" License', 'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 'Eclipse Public License 2.0', 'GNU Affero General Public License v2.0', 'GNU General Public License v2.0', 'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense' ]
     },
     {
         type: 'input',
@@ -47,7 +47,7 @@ const questions = [
 async function getInput() {
     try {
         const answers = await inquirer.prompt(questions);
-
+        console.log('answers:', answers);
         // Handle answers
 
     } catch (error) {
@@ -57,10 +57,20 @@ async function getInput() {
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(data) {
+    fs.writeFile('README.md', data, (err) => {
+        if (err) {
+            console.log('Error while writing file:', err);
+        } else {
+            console.log('README.md successfully generated!');
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    getInput();
+}
 
 // Function call to initialize app
 init();
